52ed30aa4ae8e7eb25c7e5bd569e0d35f176c62e
宋鹏 <15712847805@163.com>
2017-03-03 16:56:56 +0800

    signalR

---
+++ SingalRDemo/SingalRDemo/App_Start/Startup.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.Owin;
+using Owin;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+[assembly: OwinStartup(typeof(SingalRDemo.App_Start.Startup))]
+namespace SingalRDemo.App_Start
+{
+    public class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            app.MapSignalR();
+
+        }
+    }
+}
---
+++ SingalRDemo/SingalRDemo/Models/ChatHub.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNet.SignalR;
+using Microsoft.AspNet.SignalR.Hubs;
+
+namespace SingalRDemo.Models
+{
+    [HubName("chatHub")]
+    public class ChatHub : Hub
+    {
+        public void AddToRoom(string groupId, string userName)
+        {
+            //将分组Id放到上下文中
+            Groups.Add(Context.ConnectionId, groupId);
+            //群发人员进入信息提示
+            Clients.Group(groupId, new string[0]).addUserIn(groupId, userName);
+
+        }
+        public void Send(string groupId, string detail, string userName)
+        {
+            //Clients.All.addSomeMessage(detail);//群发给所有
+            //发给某一个组
+            Clients.Group(groupId, new string[0]).addSomeMessage(groupId, detail, userName);
+        }
+    }
+}
